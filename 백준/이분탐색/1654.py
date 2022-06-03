4159564078f129b0e3e9de6f9c5dbe65d549e501
GGE2 <jihoon_story@naver.com>
2022-06-03 17:31:17 +0900

    랜선 자르기

--- 백준/이분탐색/1654.py
+++ 백준/이분탐색/1654.py
@@ -7,6 +7,8 @@ n, k = map(int,input().split())
 nums = [int(input()) for _ in range(n)]
 start,end = 1,max(nums)
 
+
+
 while start<=end:
     lans=0
     mid = (end+start)//2
@@ -18,5 +20,6 @@ while start<=end:
         end = mid-1
     else:
         start = mid+1
+
 
 print(end)
