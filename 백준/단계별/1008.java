e6350799274a5a7a00533305d82790cb4fbe39d1
KimJiHun <68943993+GGE2@users.noreply.github.com>
2023-03-22 23:44:33 +0900

    1008

---
+++ 백준/단계별/1008.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner input=new Scanner(System.in);
+		
+		double x=input.nextInt();
+		double y=input.nextInt();
+		
+		System.out.println(x/y);
+
+	}
+
+}
