b71412e170627690e65912900007693188f249c0
KimJiHun <68943993+GGE2@users.noreply.github.com>
2023-07-09 23:10:30 +0900

    1998년생인 내가 태국에서는 2541년생!?

---
+++ 백준/단계별/18108.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(n-543);
+
+
+    }
+
+}
