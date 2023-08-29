e6d6596bd90dad5317c46ee6ddb698ac3d76ecae
KimJiHun <68943993+GGE2@users.noreply.github.com>
2023-08-29 20:21:20 +0900

    단어 길이 재기

---
+++ 백준/문자열/2743.java
@@ -0,0 +1,22 @@
+
+
+import java.io.*;
+import java.util.*;
+
+
+class Main{
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        System.out.println(str.length());
+    }
+
+
+
+}
